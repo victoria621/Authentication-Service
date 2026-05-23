1880ad7627572a6d0953bc5a3ceef2508576a970
victoria <vasharchuk07@gmail.com>
2026-05-23 08:25:25 +0300

    add repositories

---
+++ src/main/java/com/innowise/authenticationservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.innowise.authenticationservice.repository;
+
+import com.innowise.authenticationservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
+    boolean existsByLogin(String login);
+}
